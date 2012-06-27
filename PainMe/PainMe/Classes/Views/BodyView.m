9cda1f6fe8cbc4d44bf7e51a3de83677442f02b3
Dhaval <dhaval@goinvo.com>
2012-06-27 14:08:45 -0400

    New files of sizes 512, 256,1024 added and used. Scrolling is now smoother.

--- PainMe/PainMe/Classes/Views/BodyView.m
+++ PainMe/PainMe/Classes/Views/BodyView.m
@@ -42,7 +42,7 @@
 - (void) awakeFromNib {
    [super awakeFromNib];
    CATiledLayer *tiledLayer = (CATiledLayer *) self.layer;
-   
+    tiledLayer.levelsOfDetail = 3;
    //CGFloat scale = [UIScreen mainScreen].scale;
    tiledLayer.tileSize = CGSizeMake(BODY_TILE_SIZE, BODY_TILE_SIZE);
    self.contentScaleFactor = 1.0;
@@ -57,6 +57,9 @@
    
    CGSize tileSize = (CGSize){BODY_TILE_SIZE, BODY_TILE_SIZE};
    
+    CGFloat scale = CGContextGetCTM(context).a;
+//    NSLog(@"Scale in draw is %f",scale);
+    
    int firstCol = floorf(CGRectGetMinX(rect) / tileSize.width);
    int lastCol = floorf((CGRectGetMaxX(rect)-1) / tileSize.width);
    int firstRow = floorf(CGRectGetMinY(rect) / tileSize.height);
@@ -64,7 +67,7 @@
    
    for (int row = firstRow; row <= lastRow; row++) {
       for (int col = firstCol; col <= lastCol; col++) {
-         UIImage *tile = [self tileAtCol:col row:row];
+         UIImage *tile = [self tileAtCol:col row:row withScale:scale];
          
          if (tile) {
             CGRect tileRect = CGRectMake(tileSize.width * col, 
@@ -85,10 +88,21 @@
    }
 }
 
-- (UIImage*)tileAtCol:(int)col row:(int)row
+- (UIImage*)tileAtCol:(int)col row:(int)row withScale:(CGFloat)scale
 {
+    int numFrmScale = 256;
+    if (scale == 1.0) {
+        numFrmScale = 1024;
+    }
+    else if (scale == 0.5) {
+        numFrmScale = 512;
+    }
+    else {
+        numFrmScale = 256;
+    }
+    
    if (col>= 0 && col < BODY_TILE_COLUMNS && row >= 0 && row < BODY_TILE_ROWS) {
-      NSString *filename = [NSString stringWithFormat:@"body_large_%02d.png", (col+1) % BODY_TILE_COLUMNS + (row) * BODY_TILE_COLUMNS];
+      NSString *filename = [NSString stringWithFormat:@"body_large_%02d_%d.png", (col+1) % BODY_TILE_COLUMNS + (row) * BODY_TILE_COLUMNS,numFrmScale];
       NSString *path = [[NSBundle mainBundle] pathForResource: filename ofType: nil];
       UIImage *image = [_imageCache objectForKey: path];
       if (!image) {
