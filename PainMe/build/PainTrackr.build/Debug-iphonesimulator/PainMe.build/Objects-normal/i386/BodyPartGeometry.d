8e71e07f44ecac4d8ece099482d834a5ccd84718
Dhaval <dhaval@goinvo.com>
2012-08-08 12:15:18 -0400

    Apache license 2.0 and Notice files added

---
+++ PainMe/build/PainTrackr.build/Debug-iphonesimulator/PainMe.build/Objects-normal/i386/BodyPartGeometry.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/Model/BodyPartGeometry.m \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/Model/BodyPartGeometry.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/Model/PainLocation.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/InvoDataManager.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/CHCSV.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/CHCSVParser.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/CHCSVWriter.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/NSArray+CHCSVAdditions.h \
+  /Users/DDKarwa/Desktop/PainMe/PainMeTrackerRepo/PainMe/PainMe/Classes/NSString+CHCSVAdditions.h
