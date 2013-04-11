8329e7ff9275851f738c0cb6d41922598fb91657
Dhaval <dhaval@goinvo.com>
2013-04-11 12:29:22 -0400

    Added Flurry, some Code Clean Up and Bug fixes when clearing the entries at once

---
+++ PainMe/PainMe/Classes/Helpers/UIColor+PainColor.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+PainColor.h
+//  PainTrackr
+//
+//  Created by Dhaval Karwa on 4/11/13.
+//  Copyright (c) 2013 InvolutionStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (PainColor)
+
+@end
---
+++ PainMe/PainMe/Classes/Helpers/UIColor+PainColor.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+PainColor.m
+//  PainTrackr
+//
+//  Created by Dhaval Karwa on 4/11/13.
+//  Copyright (c) 2013 InvolutionStudios. All rights reserved.
+//
+
+#import "UIColor+PainColor.h"
+
+@implementation UIColor (PainColor)
+
+@end
